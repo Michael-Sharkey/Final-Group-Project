1f691c572ba9b1dc7bfbf0dcf83b1f49a4d40dec
echo 'hello world <ssmyth1071@gmail.com>
2018-01-19 22:31:58 -0500

    add rspec questions routes

--- spec/routing/questions_spec.rb
+++ spec/routing/questions_spec.rb
@@ -14,4 +14,30 @@ RSpec.describe 'routing to questions' do
       id: '1'
     )
   end
+  it 'routes /questions/new to questions#new' do
+    expect(get: "/questions/new").to route_to(
+      controller: "questions",
+      action: "new"
+    )
+  end
+  it 'routes /questions to questions#create' do
+    expect(post: "/questions").to route_to(
+      controller: "questions",
+      action: "create"
+    )
+  end
+  it "routes /questions/1 to questions#update" do
+    expect(put: "/questions/1").to route_to(
+      controller: "questions",
+      action: "update",
+      id: "1"
+    )
+  end
+  it "routes /questions/1 to questions#destroy" do
+    expect(delete: "/questions/1").to route_to(
+      controller: "questions",
+      action: "destroy",
+      id: "1"
+    )
+  end
 end
