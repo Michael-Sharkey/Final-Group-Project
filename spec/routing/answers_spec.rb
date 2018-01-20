4d8d1be80e35ea826356b80ab2ad6706d26e49c2
echo 'hello world <ssmyth1071@gmail.com>
2018-01-19 23:51:58 -0500

    update answers rpsec route tests

--- spec/routing/answers_spec.rb
+++ spec/routing/answers_spec.rb
@@ -1,43 +1,51 @@
 require 'rails_helper'
 
-RSpec.describe 'routing to answers' do
-  it 'routes /answers to answers#index' do
-    expect(get: "/answers").to route_to(
+RSpec.describe 'routing to question/answers' do
+  #TODO - need to remove route to ansers index - since that is not necessary
+  it 'routes questions/1/answers/1 to answers#edit' do
+    expect(get: "questions/1/answers/1/edit").to route_to(
       controller: "answers",
-      action: "index"
-    )
-  end
-  it 'routes /answers/1 to answers#show' do
-    expect(get: "/answers/1").to route_to(
-      controller: "answers",
-      action: "show",
+      action: "edit",
+      question_id: '1',
       id: '1'
     )
   end
-  it 'routes /answers/new to answers#new' do
-    expect(get: "/answers/new").to route_to(
+  it 'routes questions/1/answers/1 to answers#show' do
+    expect(get: "questions/1/answers/1").to route_to(
       controller: "answers",
-      action: "new"
+      action: "show",
+      question_id: '1',
+      id: '1'
     )
   end
-  it 'routes /answers to answers#create' do
-    expect(post: "/answers").to route_to(
+  it 'routes questions/1/answers/new to answers#new' do
+    expect(get: "questions/1/answers/new").to route_to(
       controller: "answers",
-      action: "create"
+      action: "new",
+      question_id: '1'
     )
   end
-  it "routes /answers/1 to answers#update" do
-    expect(put: "/answers/1").to route_to(
+  it 'routes questions/1/answers to answers#create' do
+    expect(post: "questions/1/answers").to route_to(
+      controller: "answers",
+      action: "create",
+      question_id: '1'
+    )
+  end
+  it "routes questions/1/answers/1 to answers#update" do
+    expect(put: "questions/1/answers/1").to route_to(
       controller: "answers",
       action: "update",
-      id: "1"
+      id: "1",
+      question_id: '1'
     )
   end
-  it "routes /answers/1 to answers#destroy" do
-    expect(delete: "/answers/1").to route_to(
+  it "routes questions/1/answers/1 to answers#destroy" do
+    expect(delete: "questions/1/answers/1").to route_to(
       controller: "answers",
       action: "destroy",
-      id: "1"
+      id: "1",
+      question_id: '1'
     )
   end
 end
